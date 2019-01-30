819bfb5b798b67b03aeae674b1690862c7a3d03d
TatianaMayskaya <tmayskaya@gmail.com>
2019-01-31 02:42:53 +0300

    31_Jan_19

--- CT19_OptimalTaxation/models.py
+++ CT19_OptimalTaxation/models.py
@@ -20,21 +20,45 @@ class Constants(BaseConstants):
     num_rounds = 30  # maximum number of participants allowed
 
 
+def utility_function(p, q, theta):
+    arg = q - q * q / 2 / theta - p
+    return 10*(1 - math.exp(-arg))
+
+
 class Subsession(BaseSubsession):
     def creating_session(self):
         if self.round_number == 1:
             players = self.get_players()
             for p in players:
                 p.theta = self.session.config['thetaH']
+                p.participant.vars['income_1'] = '-'
+                p.participant.vars['tax_1'] = '-'
+                p.participant.vars['income_2'] = '-'
+                p.participant.vars['tax_2'] = '-'
             low_type = random.sample(players, int(self.session.config['beta'] * len(players)))
             for p in low_type:
                 p.theta = self.session.config['thetaL']
             self.session.vars['num_rounds'] = len(players)
+            self.session.vars['n_low'] = len(low_type)
+            self.session.vars['n_high'] = len(players) - len(low_type)
 
+    def vars_for_admin_report(self):
+        results_table = []
+        players = self.get_players()
+        for p in players:
+            results_table.append(((p.id_in_group,
+                                   p.participant.vars['income_1'],
+                                   p.participant.vars['tax_1'],
+                                   p.participant.vars['income_2'],
+                                   p.participant.vars['tax_2'])))
+        return {'results_table': results_table}
 
 
 class Group(BaseGroup):
-    pass
+    income_1 = models.FloatField(min=0)
+    tax_1 = models.FloatField()
+    income_2 = models.FloatField(min=0)
+    tax_2 = models.FloatField()
 
 
 class Player(BasePlayer):
@@ -44,4 +68,4 @@ class Player(BasePlayer):
     income_2 = models.FloatField(min=0)
     tax_2 = models.FloatField()
     income = models.FloatField(min=0)
-
+    tax = models.FloatField()
--- CT19_OptimalTaxation/tests.py
+++ CT19_OptimalTaxation/tests.py
@@ -7,4 +7,7 @@ from .models import Constants
 class PlayerBot(Bot):
 
     def play_round(self):
-        pass
+        if self.round_number == 1:
+            yield (pages.Government, {'income_1': 0.5, 'tax_1': 0.3, 'income_2': 0.2, 'tax_2': 4})
+        if self.round_number <= self.session.vars['num_rounds']:
+            yield (pages.Worker, {'income': 0.5})
